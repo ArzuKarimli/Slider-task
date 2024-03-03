4865469345b53fae711910eeb87f31af2ed575fd
arzusk <arzusk@code.edu.az>
2024-03-02 19:56:12 -0800

    added codes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,34 @@
 let leftIcon = document.querySelector(".left-icon");
 let rightIcon = document.querySelector(".right-icon");
+let images = document.querySelectorAll("img");
+
+for (const img of images) {
+    img.classList.add("right");
+}
 
 function rightSlider() {
     let activeImage = document.querySelector(".active");
-    activeImage.classList.remove("active");
-    if (activeImage.nextElementSibling != null) {
-        activeImage.nextElementSibling.classList.add("active");
+    activeImage.classList.remove("active", "left");
 
+    if (activeImage.nextElementSibling != null) {
+        activeImage.nextElementSibling.classList.add("active", "right");
     } else {
-        rightIcon.parentNode.nextElementSibling.firstElementChild.classList.add("active");
+        images[0].classList.add("active", "right");
     }
 }
+
 function leftSlider() {
     let activeImage = document.querySelector(".active");
-    activeImage.classList.remove("active");
-    if (activeImage.previousElementSibling != null) {
-        activeImage.previousElementSibling.classList.add("active");
+    activeImage.classList.remove("active", "right");
 
+
+    if (activeImage.previousElementSibling != null) {
+        activeImage.previousElementSibling.classList.add("active", "left");
     } else {
-        leftIcon.parentNode.nextElementSibling.lastElementChild.classList.add("active");
+        images[0].classList.add("active", "left");
     }
 }
 
-
 rightIcon.addEventListener("click", rightSlider);
-leftIcon.addEventListener("click", leftSlider);
+// leftIcon.addEventListener("click", leftSlider);
+
